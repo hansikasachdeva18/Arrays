c7b986837cd42e3f46304565a72eaa50a37c692a
Hansika Sachdeva <86574049+hansikasachdeva18@users.noreply.github.com>
2025-06-27 12:33:27 +0530

    Update 3sum.java

--- 3sum.java
+++ 3sum.java
@@ -23,3 +23,36 @@ public class Solution {
 }
 
 // because we do not want to store duplicates and we know HashSet helps in storing unique so we will use HashSet but here we need to store the list of elemenets.
+
+
+Better Solution using only 2 loops 
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) { 
+         Set<List<Integer>> st = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            Set<Integer> hashset = new HashSet<>();
+            for (int j = i + 1; j < nums.length; j++) {
+                //Calculate the 3rd element:
+                int third = -(nums[i] + nums[j]);
+
+                //Find the element in the set:
+                if (hashset.contains(third)) {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], third);
+                    temp.sort(null);
+                    st.add(temp);
+                }
+                hashset.add(nums[j]);
+            }
+        }
+
+        // store the set elements in the answer:
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+        
+
+    }
+}
+
+
