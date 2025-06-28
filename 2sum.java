9b2fccf839dd58eaf68aa0ab869cfd1a01362c2f
Hansika Sachdeva <86574049+hansikasachdeva18@users.noreply.github.com>
2025-06-28 07:45:28 +0530

    Create 2sum.java

---
+++ 2sum.java
@@ -0,0 +1,65 @@
+Brute 
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans=new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+
+Better 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       HashMap<Integer, Integer> map = new HashMap<>();
+       int arr[]= new int[2];
+       for(int i=0;i<nums.length;i++){
+        int value = nums[i];// 3, 3
+        int ans=target-value; // 3, 3
+        if(map.containsKey(ans)) {
+            arr[0]=i; //1
+            arr[1]=map.get(ans);
+            return arr;
+        }
+        map.put(nums[i], i);// 3,0
+       }
+
+       return arr;
+    }
+}
+
+
+Optimal 
+
+ Arrays.sort(nums);
+        int[] ans=new int[2];
+         int i=0;
+         int j=nums.length-1;
+        
+        while(i<j){
+             int sum=nums[i]+nums[j];
+             if(sum==target){
+                 ans[0]=i;
+                 ans[1]=j;
+                 return ans;
+             }
+             else{
+                 if(sum>target){
+                     j--;
+                 }
+                 else{
+                     i++;
+                 }
+             }
+         }
+
+         return ans;
