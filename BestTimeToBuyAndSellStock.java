c21ea8130d4f02058e5c2214026ccec5ed9df135
Hansika Sachdeva <86574049+hansikasachdeva18@users.noreply.github.com>
2025-07-01 23:44:28 +0530

    Update BestTimeToBuyAndSellStock.java

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -19,3 +19,24 @@ class Solution {
     }
         
 }
+
+Optimal : 
+class Solution {
+    public int maxProfit(int[] prices) {
+    
+    int maxProfit = 0;
+    int min = prices[0];
+
+    for(int i=1;i<prices.length;i++){
+        int profit = prices[i]-min;
+        if(profit>maxProfit) maxProfit = profit;
+        if(prices[i]<min) min = prices[i];
+    }
+
+
+    return maxProfit;
+       
+    }
+        
+}
+
